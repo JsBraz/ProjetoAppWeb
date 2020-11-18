40f16279c84401570d978e4708ce3bb290788225
Josue <josuepbraz@gmail.com>
2020-11-18 19:55:10 +0000

    implemented no duplicate user register + gorm annotations for user model

--- backend/model/Users.go
+++ backend/model/Users.go
@@ -4,7 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Users struct {
 	gorm.Model `swaggerignore:"true"`
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	IsAdmin    bool   `json:"isAdmin"`
+	Username   string `gorm:"unique;not null" json:"username"`
+	Password   string `gorm:"not null" json:"password"`
+	IsAdmin    bool   `gorm:"not null" json:"isAdmin"`
 }
